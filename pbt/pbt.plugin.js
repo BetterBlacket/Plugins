f3eb0042f1654c10ba09e2f2fc55022a417662a3
VR <101288516+VillainsRule@users.noreply.github.com>
2023-03-27 17:44:58 -0400

    readd test plugin

---
+++ pbt/pbt.plugin.js
@@ -0,0 +1,9 @@
+/**
+ * @name Test Plugin
+ * @authorId 3324853
+ * @version 1.0.0
+ * @description Whar is this?
+ * @source https://github.com/VillainsRule/BB-Plugins/blob/main/pbt
+ */
+
+alert('hi');
