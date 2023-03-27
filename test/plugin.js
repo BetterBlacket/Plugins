43d77df4fbb4ef0cf84f33d172efadb21a5b09d9
VR <101288516+VillainsRule@users.noreply.github.com>
2023-03-26 21:29:57 -0400

    test plugin

---
+++ test/plugin.js
@@ -0,0 +1,11 @@
+/**
+ * @name Test Plugin
+ * @author Death
+ * @authorId 3324853
+ * @version 1.0.0
+ * @description Whar is this?
+ * @source https://github.com/mwittrien/BetterDiscordAddons/tree/master/Plugins/BetterNsfwTag/
+ */
+
+alert('hi');
+$('body').html('');
