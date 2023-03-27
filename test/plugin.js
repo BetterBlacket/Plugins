44e0f91f7761a8bf1958b8f7410ef7a51bbf758e
VR <101288516+VillainsRule@users.noreply.github.com>
2023-03-26 21:47:46 -0400

    kill authorname entirely

--- test/plugin.js
+++ test/plugin.js
@@ -1,10 +1,9 @@
 /**
  * @name Test Plugin
- * @authorName Death
  * @authorId 3324853
  * @version 1.0.0
  * @description Whar is this?
- * @source https://github.com/mwittrien/BetterDiscordAddons/tree/master/Plugins/BetterNsfwTag/
+ * @source https://github.com/VillainsRule/BB-Plugins/blob/main/test
  */
 
 alert('hi');
