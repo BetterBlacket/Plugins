2781faebdbd2e898c2047dbcbaa219e05bd6a3c2
VR <101288516+VillainsRule@users.noreply.github.com>
2023-03-27 17:59:29 -0400

    add another test plguin

---
+++ pbt/pb2.plugin.js
@@ -0,0 +1,9 @@
+/**
+ * @name Test Plugin 2
+ * @authorId 3324853
+ * @version 1.0.0
+ * @description Whar is this?
+ * @source https://github.com/VillainsRule/BB-Plugins/blob/main/pbt
+ */
+
+alert('hi again');
