41333783c3272d80bc15d5ddd488e1648f7bf572
VR <101288516+VillainsRule@users.noreply.github.com>
2023-05-01 17:42:29 -0400

    Update pbt.plugin.js

--- pbt/pbt.plugin.js
+++ pbt/pbt.plugin.js
@@ -4,6 +4,8 @@
  * @version 1.0.0
  * @description Whar is this?
  * @source https://github.com/VillainsRule/BB-Plugins/blob/main/pbt
+ * @require ["https://raw.githubusercontent.com/VillainsRule/BB-Plugins/main/pbt/pbt.plugin.js","https://raw.githubusercontent.com/VillainsRule/BB-Plugins/main/pbt/pbt.plugin.js"]
+ * @homepage https://blacket.org
  */
 
 console.log('hi');
